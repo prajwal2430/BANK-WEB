dbec0a76546a14dfdac8c0bbdc0e505e944f88b8
prajwal2430 <prajwalpatil1008p@gmail.com>
2026-04-22 23:48:08 +0530

    fix errors

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const cors    = require('cors');
 const connectDB = require('./config/db');
 
+const app = express();
+
 // Middleware
 app.use(cors());
 app.use(express.json());
